ca78441579aacf451bacaf859d9b45edeaf4f084
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-10-16 18:15:29 +0100

    Refactored a plane double in passenger spec

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,20 +1,25 @@
 require "passenger"
 
 describe Passenger do
+
+  let(:plane) { double(:plane, full?: false, seats: []) }
+
   it "has a checked in status" do
     expect(subject).to respond_to(:checked_in?)
   end
+
   it "can be checked in" do
     expect(subject.check_in).to be_checked_in
   end
+
   it "can board a plane if checked in" do
-    plane = Plane.new
     subject.check_in
     subject.board!(plane)
     expect(subject).to be_on_board
   end
+
   it "cannot board a plane if not checked in" do
-    plane = Plane.new
     expect { subject.board!(plane) }.to raise_error "Check in before boarding"
   end
+
 end
